70f98907b50fb75261c911b5dca8d987dcfedbc3
CRQ <achenruiqiluigi@gmail.com>
2025-04-27 20:45:05 +0200

    Update

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import pyautogui as pgui
-import os
 import time
 import pyperclip
+import pytesseract
+from PIL import Image
+pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'
 
+
+def get_imgmsg(imgname):
+    region = (380, 150, 500, 620)
+    screenshot= pgui.screenshot(region=region)
+    screenshot.save(imgname)
+
+def PreprocessingImg(imgname):
+    img = Image.open(imgname)
+    gray_img=img.convert('L')
+    threshold = 128
+    binary_img = gray_img.point(lambda p: p > threshold and 255)
+    binary_img.save(imgname)
+
+def getTextFromImg(imgname)->str:
+    # 加载图像
+    img = Image.open(imgname)
+    img.show()
+    # 使用 Tesseract 进行 OCR 识别
+    text = pytesseract.image_to_string(img,lang='chi_sim')
+
+    # 打印识别到的文本
+    return text
 
 def getmouseposition():
-    time.sleep(3)
+    time.sleep(4)
     print(pgui.position())
-    exit()
+    #exit()
 
 def initprompt():
     print("把微信框放在左上 并到左上框里 不要遮挡\n")
@@ -15,20 +39,33 @@ def initprompt():
         print(f"after {i} sec will START" ,end='\r')
         time.sleep(1)
 
+def getContentFromMyChat()-> str:
+    pgui.moveTo(130,65)#serch bar
+    pgui.click()
+    content="文件传输助手"
+    pyperclip.copy(content)
+    pgui.hotkey('command','v')
+    pgui.typewrite("\n")
+    pgui.moveTo(600,900)#text box
+    pgui.rightClick()
+    pgui.moveTo(1380,757)
+    pgui.click()
+    return pyperclip.paste()
+    
+
 def doGrupBroadcast():
     initprompt()
 
     #set time pause
     pgui.PAUSE=1
     #content set
-    content="你好" \
-    "sd"
+    content=getContentFromMyChat()
     serch_part="BO grp"
-    ran = 1
+    ran = 10
     #action
     ran= ran+1
     for num in range(1,ran):
-        pgui.moveTo(116,65)
+        pgui.moveTo(116,65)#serch bar
         pgui.click()
         serch=f"{serch_part} {num}"
         pgui.typewrite(serch)
@@ -38,6 +75,14 @@ def doGrupBroadcast():
         pyperclip.copy(content)
         pgui.hotkey('command','v')
 
+def parse_img():
+    imgname="temp.png"
+    get_imgmsg(imgname)
+    PreprocessingImg(imgname)
+    text=getTextFromImg(imgname)
+    print(text)
 
 doGrupBroadcast()
-getmouseposition()
+#getmouseposition()
+#parse_img()
+#then conn to llm to get complete and good format text
